c9fae16c472d982ca27047facb4881e72bfdb7b4
Fred-txd <piustxd@gmail.com>
2023-03-28 10:08:25 -0700

    Fixed requiremets error

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 class square:
-    
+
     def __init__(self, size):
+
         '''new square initialization
         args:
         size (int): the size of a new square
         '''
+
         self.__size = size
