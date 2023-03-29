e57040f02f6af85c77670b315adc2d302d9dc20f
Fred-txd <piustxd@gmail.com>
2023-03-29 07:57:33 -0700

    Exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -12,10 +12,10 @@
 
 def safe_print_list(my_list=[], x=0):
     i = 0
+    while i < x
     try:
-        while i < x:
-            print(my_list[i], end='')
-            i += 1
+        print(my_list[i], end='')
+        i += 1
     except IndexError:
         pass
     finally:
