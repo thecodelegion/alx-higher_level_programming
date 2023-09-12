e7febf410564db51b070e20afdc453ee809b6ff4
Fred-txd <piustxd@gmail.com>
2023-09-13 00:54:35 +0800

    Task 2 Done

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
-const count = process.argv.length;
-console.log(
-  count === 2
-    ? 'No argument'
-    : count === 3
-      ? 'Argument found'
-      : 'Arguments found'
-);
+const process = require('process');
+
+const args = process.argv.slice(2); // Get the arguments passed to the script
+
+if (args.length === 0) {
+  console.log('No argument');
+} else if (args.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
