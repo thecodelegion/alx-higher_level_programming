124d79c7885412bb25fb38019541a72e4a199520
Code Legion <piustxd@gmail.com>
2023-10-01 16:44:45 +0800

    Delete 0x0E-SQL_more_queries directory

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++
@@ -1,3 +0,0 @@
--- creates the table unique_id on my MySQL server
--- and creates a table
-CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
--- 0x0E-SQL_more_queries/README.md
+++
@@ -1,1 +0,0 @@
-More on SQL Queries
